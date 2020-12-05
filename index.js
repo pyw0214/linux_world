8801a3aa6494eb0b106d2739f19c7b3f026f3442
pyw0214 <yongwookp@gmail.com>
2020-12-05 21:44:38 +0900

    Updated_201205

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-const age = prompt ("How old are you?");
+const title = document.querySelector("#title");
 
-if (age >= 18 && age <= 21) {
-    console.log('you can drink but you should not');
-}else if(age > 21){
-    console.log ("go ahead"); 
-} else {
-    console.log("too young!")
+const BASE_COLOR = "white"
+const OTHER_COLOR = "#6c5ce7";
+
+function handleClick(){
+    const currentColor = title.style.color;
+    if (currentColor === BASE_COLOR){
+        title.style.color = OTHER_COLOR;
+    } else {
+        title.style.color = BASE_COLOR;
+
+    }
 }
+
+function init(){
+    title.style.color = BASE_COLOR;
+}
+
+title.addEventListener("mouseenter", handleClick);
+
+init();
+
