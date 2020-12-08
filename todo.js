b8d8571bea5e13f807e33bc00086f89682f66b79
pyw0214 <yongwookp@gmail.com>
2020-12-08 20:50:01 +0900

    updated_1208

--- todo.js
+++ todo.js
@@ -4,8 +4,23 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = 'toDos';
 
+function filterFn(toDo){
+    return toDo.id === 1
+}
 
-const toDos = [];
+let toDos = [];
+
+function deleteToDo(event){
+    const btn  = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo){
+        return toDo.id !==parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+}
+
 
 function saveToDos(){
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -14,9 +29,11 @@ function saveToDos(){
 function paintToDo(text){
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
-    delBtn.innerText = "X";
+  
     const span = document.createElement("span");
     const newId = toDos.length + 1;
+    delBtn.innerText = "X";
+    delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;
     li.appendChild(span);
     li.appendChild(delBtn);
